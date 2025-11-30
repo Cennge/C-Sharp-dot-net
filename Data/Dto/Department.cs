7d0d2c8f09c770920b526db3ec29d2879f62a864
Cennge <ilyagliba02@gmail.com>
2025-12-01 00:56:48 +0200

    done

---
+++ Data/Dto/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_all.Data.Dto
+{
+    [csharp_all.Data.Attributes.TableName("tbl_Departments")]
+    internal class Department
+    {
+        public Guid Id { get; set; }
+        public String Name { get; set; } = null!;
+
+        public override string? ToString()
+        {
+            String id = Id.ToString();
+            return $"{id[..3]}...{id[^3..]} {Name}";
+        }
+    }
+}
---
+++ Data/Dto/Sale.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_all.Data.Dto
+{
+    [csharp_all.Data.Attributes.TableName("tbl_Sales")]
+    internal class Sale
+    {
+        public Guid Id { get; set; }
+        public Guid ManagerId { get; set; }
+        public Guid ProductId { get; set; }
+        public DateTime Moment { get; set; }
+        public int Quantity { get; set; }
+    }
+}
